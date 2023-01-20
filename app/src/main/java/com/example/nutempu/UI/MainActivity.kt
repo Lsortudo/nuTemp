534c43bdda3090ef189a84ab35d373f306c83347
L <leonardo.alvirubro@hotmail.com>
2023-01-19 23:10:55 -0300

    build: MVVM folder architecture

--- app/src/main/java/com/example/nutempu/UI/MainActivity.kt
+++ app/src/main/java/com/example/nutempu/UI/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.nutempu
+package com.example.nutempu.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nutempu.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
