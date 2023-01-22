956d580f7fe07a64498a735c2db0710420d73399
L <leonardo.alvirubro@hotmail.com>
2023-01-21 23:26:44 -0300

    feat: Add package ( bottom sheet dialog ) basic logic created

--- app/src/main/java/com/example/nutempu/UI/MainActivity.kt
+++ app/src/main/java/com/example/nutempu/UI/MainActivity.kt
@@ -3,10 +3,33 @@ package com.example.nutempu.UI
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nutempu.R
+import com.example.nutempu.databinding.ActivityMainBinding
+import com.example.nutempu.databinding.AddPackageSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNewPackage.setOnClickListener {
+            showBtnSheetDialog()
+        }
+
+    }
+
+    private fun showBtnSheetDialog() {
+        val dialog = BottomSheetDialog(this)
+
+        val sheetBinding: AddPackageSheetBinding =
+            AddPackageSheetBinding.inflate(layoutInflater, null, false)
+
+        dialog.setContentView(sheetBinding.root)
+        dialog.show()
     }
 }
